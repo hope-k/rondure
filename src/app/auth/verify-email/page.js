1ee149dc063ccdefb552c063ce15eb49e15a9692
Hope Kumordzie <hopekumordzie@gmail.com>
2024-12-16 19:15:58 -0500

    auth addedxx

--- src/app/auth/verify-email/page.js
+++ src/app/auth/verify-email/page.js
@@ -2,17 +2,22 @@
 import VerifyEmail from '@/components/Auth/VerifyEmail';
 import { motion } from "framer-motion";
 
+import { Suspense } from 'react';
+
 export default function Page() {
     return (
-        <motion.div
-            initial={{ opacity: 0, y: 10 }} // Start further down for a subtle drop-in effect
-            animate={{ opacity: 1, y: 0 }} // Smoothly animate to its natural position
-            transition={{
-                duration: 0.6, // Slightly longer duration
-                ease: [0.4, 0, 0.2, 1], // Custom cubic bezier for a polished ease
-            }}
-        >
-            <VerifyEmail />
-        </motion.div>
+        <Suspense>
+            <motion.div
+                initial={{ opacity: 0, y: 10 }} // Start further down for a subtle drop-in effect
+                animate={{ opacity: 1, y: 0 }} // Smoothly animate to its natural position
+                transition={{
+                    duration: 0.6, // Slightly longer duration
+                    ease: [0.4, 0, 0.2, 1], // Custom cubic bezier for a polished ease
+                }}
+            >
+                <VerifyEmail />
+            </motion.div>
+
+        </Suspense>
     );
 }
